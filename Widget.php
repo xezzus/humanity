f54c019acd638353da7d25f94f4e0fc414080ad3
Andrey <xezzus@gmail.com>
2015-05-06 17:43:40 +0300

    Developing

--- Widget.php
+++ Widget.php
@@ -20,9 +20,9 @@ class Widget {
         return new self;
     }
 
-    public function __call($name,$value){
+    public function __call($_name,$value){
         if(isset($value[0]) && !empty($value[0]) && is_array($value[0])) extract($value[0]);
-        $file = (new Config)->instance()->config['paths']['widget'].'/'.self::$property.'/'.$name.'.phtml';
+        $file = (new Config)->instance()->config['paths']['widget'].'/'.self::$property.'/'.$_name.'.phtml';
         if(is_file($file)) require($file);
     }
 
