756dfbaaecd91780c05bfb668e41dd58e392d4b9
Andrey <xezzus@gmail.com>
2015-05-30 21:44:41 +0300

    Developing

--- api.sql
+++ api.sql
@@ -1,10 +1,24 @@
 PRAGMA foreign_keys=OFF;
 BEGIN TRANSACTION;
 CREATE TABLE "method" (
-  "name" text NOT NULL,
-  "status" text NOT NULL DEFAULT 'PUBLIC',
-  "params" text NULL,
-  PRIMARY KEY ("name")
+    "name" text NOT NULL,
+    "status" text NOT NULL DEFAULT 'PUBLIC',
+    "params" text NULL
 );
+CREATE UNIQUE INDEX "method_name" ON "method" ("name");
 CREATE INDEX "method_status" ON "method" ("status");
+
+CREATE TABLE "permission" (
+    "app_id" integer NOT NULL,
+    "method" text NOT NULL
+);
+CREATE INDEX "permissions_app_id" ON "permission" ("app_id");
+CREATE UNIQUE INDEX "permissions_unique" ON "permission" ("app_id","method");
+
+CREATE TABLE "app" (
+    "id" integer NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "sig" text NOT NULL,
+    "description" text NOT NULL
+);
+CREATE UNIQUE INDEX "app_id_sig" ON "app" ("id","sig");
 COMMIT;
