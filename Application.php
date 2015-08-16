6fbf436c727cbd38290ed89c6fec51fb288ecffe
Andrey-xezzus <xezzus@gmail.com>
2015-08-16 15:35:18 +0300

    Developing

--- Application.php
+++ Application.php
@@ -3,24 +3,26 @@ namespace humanity;
 
 class Application {
 
-    private static $singleton;
+    private static $memory = [];
     private static $config;
-    private static $name;
     private static $accept;
+    private static $application = [];
 
-    public function __construct($name=null){
-        self::$name = $name;
-        self::$singleton = (new Singleton)->instance();
+    public function __construct(){
         self::$config = (new Config)->get();
         self::$accept = (new Accept)->instance();
     }
 
     public function __get($name){
-        return new Application($name);
+        array_push(self::$application,$name);
+        return (new self);
     }
 
     public function __call($name,$value){
-        $file = self::$config['paths']['apps'].'/'.self::$name.'/'.$name.'.php'; self::$name = null;
+        array_push(self::$application,$name);
+        $file = implode('/',self::$application);
+        $file = self::$config['paths']['apps'].'/'.$file.'.php';
+        self::$application = [];
         if(is_file($file)) {
             $result = require($file);
             if(is_callable($result)) $result = call_user_func_array($result,$value);
--- Site.php
+++ Site.php
@@ -4,9 +4,11 @@ namespace humanity;
 class Site {
 
     private static $accept;
+    private static $config;
 
     public function __construct(){
 
+        self::$config = (new Config)->get();
         self::$accept = (new Accept)->instance();
 
         header('Access-Control-Allow-Origin: *');
@@ -21,7 +23,27 @@ class Site {
             (new RestApi)->widget();
         } else if(isset(self::$accept->application['apps'])){
             header('Content-Type: application/json');
-            (new RestApi)->apps();
+            $query = json_decode(file_get_contents('php://input'),1);
+            $result = [];
+            foreach($query['method'] as $method=>$params){
+                $stableParams = [];
+                $file = self::$config['paths']['apps'].'/'.$method.'.php';
+                $func = require_once($file);
+                $reflection = new \ReflectionFunction($func);
+                foreach($reflection->getParameters() as $key=>$value){
+                    if(isset($params[$value->name])) $stableParams[$value->name] = $params[$value->name];
+                    else $stableParams[$value->name] = null;
+                }
+                $name = explode('/',$method);
+                $count = count($name)-1;
+                $app = new Application;
+                foreach($name as $key=>$var){
+                    if($count == $key) $app = call_user_method_array($var,$app,$stableParams);
+                    else $app = $app->{$var};
+                }
+                $result['method'][$method] = $app;
+            }
+            die(json_encode($result));
         } else {
             (new Content)->page();
         }
