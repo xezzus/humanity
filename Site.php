221395c902a819a595508bfb8a549ec9cab0ad2f
Andrey-xezzus <xezzus@gmail.com>
2016-01-03 14:37:21 +0300

    Developing

--- Site.php
+++ Site.php
@@ -28,6 +28,23 @@ class Site {
         $requestUri = parse_url($_SERVER['REQUEST_URI']);
         if($requestUri['path'] == '/javascript.js'){
             header('Content-Type: application/javascript');
+
+            $tsstring = 'Sun, 03 Jan 2016 11:22:20 GMT';
+            $etag = md5($tsstring);
+            $if_modified_since = isset($_SERVER['HTTP_IF_MODIFIED_SINCE']) ? $_SERVER['HTTP_IF_MODIFIED_SINCE'] : false;
+            $if_none_match = isset($_SERVER['HTTP_IF_NONE_MATCH']) ? $_SERVER['HTTP_IF_NONE_MATCH'] : false;
+            if ((($if_none_match && $if_none_match == $etag) || (!$if_none_match)) &&
+                ($if_modified_since && $if_modified_since == $tsstring))
+            {
+                header('HTTP/1.1 304 Not Modified');
+                exit();
+            }
+            else
+            {
+                header("Last-Modified: $tsstring");
+                header("ETag: $etag");
+            }
+            
             if(isset($requestUri['query'])){
                 $query = explode(':',$requestUri['query']);
                 (new Content)->js($query);
